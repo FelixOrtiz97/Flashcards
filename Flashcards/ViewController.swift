d9872e679a6320bb1c0ea7221277e522ec4ce893
Felix Ortiz <felix.alexis.ortiz@gmail.com>
2022-10-04 20:33:48 -0500

    Adding cancel and done button. Nav panel. Working on correcting known issues.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -28,7 +28,19 @@ class ViewController: UIViewController {
         }
         
     }
-    func updateFlashcard(question: String, answer: String) {
-        dismiss(animated: true)
+
+    func updateFlashcard(question: String, answer: String ) {
+        //Do stuff here
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //We know the destination of the segue is the navigation Controller
+        let navigationController = segue.destination as! CreationViewController
+        
+        //We know the Navigation Controller only contains a Creation View Controller
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        //We set the flashcardsController property to self
+        creationController.flashcardsController = self
     }
 }
